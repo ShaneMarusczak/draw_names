2e551f8e3457e12b6673fa1ef26e1cd6a3a0f485
Shane <marusczaks@gmail.com>
2021-11-15 21:42:30 -0500

    more concise code

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::io;
 fn main() {
     let mut names = Vec::<String>::new();
 
-    println!("Draw random names from a hat!");
-    println!("Please type in one name at a time, then hit enter to add the next name.");
-    println!("Enter \"DONE\" when complete!");
+    println!("Draws one or more random names from a list!");
+    println!("Enter one name at a time.");
+    println!("Enter \"DONE\" when complete.");
 
     loop {
         let mut name = String::new();
@@ -15,40 +15,33 @@ fn main() {
             .read_line(&mut name)
             .expect("Failed to read line");
 
-        match name.as_str().trim() {
+        match name.trim() {
             "DONE" => break,
-            _ => {
-                names.push(String::from(name.trim()));
-            }
+            _ => names.push(String::from(name.trim())),
         }
     }
+    let names_len = names.len();
 
     let mut winner_count_str = String::new();
     let mut winner_count_num: usize = 0;
-    while winner_count_num == 0 || winner_count_num > names.len() {
+    while winner_count_num == 0 || winner_count_num > names_len {
         println!("How many names would you like to draw?");
 
         io::stdin()
             .read_line(&mut winner_count_str)
             .expect("Failed to read line");
         winner_count_num = match winner_count_str.trim().parse() {
-            Ok(num) => {
-                if num > names.len() {
-                    winner_count_str.clear();
-                    println!("Cant have more winners than names!");
-                }
-                num
-            }
-            Err(_) => {
-                winner_count_str.clear();
-                println!("Please input a non-zero positive number");
-                0
-            }
+            Ok(num) => num,
+            Err(_) => 0,
         };
+        if winner_count_num == 0 || winner_count_num > names_len {
+            winner_count_str.clear();
+            println!("Invalid Input");
+        }
     }
 
     let mut rng = thread_rng();
-    let mut choices: Vec<usize> = (0..names.len()).collect();
+    let mut choices: Vec<usize> = (0..names_len).collect();
     choices.shuffle(&mut rng);
     for num in 0..winner_count_num {
         println!("{}", names[choices[num]]);
